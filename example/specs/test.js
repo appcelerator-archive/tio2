1a87eddb69a467a0c2f2ee819764bf0579ecef6d
Jeff Haynie <jhaynie@appcelerator.com>
2014-06-05 01:06:18 -0700

    expand test cases, use latest ioslib which attempts to fix logging issue

--- example/specs/test.js
+++ example/specs/test.js
@@ -3,6 +3,23 @@ describe("test", function(){
         var view = Ti.UI.createView();
         should(view).be.not.null;
     });
+    it("should test view with parameters",function(){
+        var view = Ti.UI.createView({
+            backgroundColor:'red',
+            width: 100,
+            height: 100
+        });
+        should(view).be.not.null;
+    });
+    it("should test view with custom parameters",function(){
+        var view = Ti.UI.createView({
+            a:1,
+            b:true,
+            c:'a',
+            d:{}
+        });
+        should(view).be.not.null;
+    });
     it("should test window", function(done){
     	var win = Ti.UI.createWindow();
         var view = Ti.UI.createView();
@@ -13,6 +30,20 @@ describe("test", function(){
         }
         win.add(view);
         win.addEventListener('open',opened);
+        win.open();
+    });
+    it("should test postlayout event", function(done){
+        var win = Ti.UI.createWindow();
+        var view = Ti.UI.createView();
+        should(view).be.not.null;
+        function postlayout(event) {
+            should(event).be.not.null;
+            should(event.rect).be.object;
+            should(event.size).be.object;
+            done();
+        }
+        win.add(view);
+        win.addEventListener('postlayout',postlayout);
         win.open();
     });
 });
