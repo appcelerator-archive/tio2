4e08fe909815ae9ff5901327e6b6b346f6ef8ad6
Ingo Muschenetz <ingo@Grace.local>
2015-03-08 22:24:41 -0700

    Add sample test to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,13 +3,21 @@ module.exports = function (grunt) {
 	// Project configuration.
 	grunt.initConfig({
 		appcJs: {
-            src: ['Gruntfile.js', 'bin', 'lib/**/*.js']
+			src: ['Gruntfile.js', 'bin', 'lib/**/*.js']
 		}
+
 	});
 
 	// Load grunt plugins for modules
 	grunt.loadNpmTasks('grunt-appc-js');
 
+	grunt.registerTask('sample', 'Run sample tio2 project', function () {
+		grunt.util.spawn({
+			cmd: './bin/tio2',
+			args: ['./example', '--platform', 'ios']
+		}, grunt.task.current.async());
+	});
+
 	// register tasks
-	grunt.registerTask('default', ['appcJs']);
+	grunt.registerTask('default', ['appcJs', 'sample']);
 };
